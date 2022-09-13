80c6d2b301eadf284f393a1f900ce1386cea9dc1
DenisRudiy <112755166+DenisRudiy@users.noreply.github.com>
2022-09-13 09:42:25 +0300

    Add files via upload

---
+++ lab1/Lab_1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+
+
+public class Random_Crypto
+{
+    public static byte[] GenerateRandomNumber()
+    {
+        using (var randomNumberGenerator = RandomNumberGenerator.Create())
+        {
+            int bit = 64;
+            var randomNumber = new byte[bit];
+            randomNumberGenerator.GetBytes(randomNumber);
+            return randomNumber;
+        }
+    }
+    public static void Main()
+    {
+        int c = 1;
+        for (int i = 0; i < 13; i++)
+        {
+            var rnd = GenerateRandomNumber();
+            Console.WriteLine(c + ".");
+            Console.WriteLine(Convert.ToBase64String(rnd));
+            c++;
+        }
+        Console.ReadLine();
+    }
+}
---
+++ lab1/Lab_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Program_RND
+{
+    public static void Main()
+    {
+        int seed;
+        seed = Convert.ToInt32(Console.ReadLine());
+        Random rnd = new Random(seed);
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(rnd.Next(1, 20));
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
